99c85468efeed7db2da1a8e7a6db499d551bfedb
ken-activety <kennerous4@gmail.com>
2025-03-05 15:30:19 +0100

    average

--- ken.js
+++ ken.js
@@ -1,5 +1,18 @@
-function calculateSum(a, b) {
-    return a + b;
+// function calculateSum(a, b) {
+//     return a + b;
+// }
+// console.log(calculateSum(15, 25)); // Outputs: 40
+// console.log(calculateSum(2.5, 3.7));
+
+function calculateAverage(numbers) {
+    let total = 0; 
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i]; 
+    }
+    return total / numbers.length;
 }
-console.log(calculateSum(15, 25)); // Outputs: 40
-console.log(calculateSum(2.5, 3.7));
+
+let exampleGrades = [15, 18, 12, 20]; 
+let average = calculateAverage(exampleGrades); 
+console.log("The average of the grades is: " + average); 
+
