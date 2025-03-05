331ec285dabd5b6b54a88374372633eb22b438ad
ken-activety <kennerous4@gmail.com>
2025-03-05 15:34:04 +0100

    calculate-average

--- ken.js
+++ ken.js
@@ -4,15 +4,27 @@
 // console.log(calculateSum(15, 25)); // Outputs: 40
 // console.log(calculateSum(2.5, 3.7));
 
+// function calculateAverage(numbers) {
+//     let total = 0; 
+//     for (let i = 0; i < numbers.length; i++) {
+//         total += numbers[i]; 
+//     }
+//     return total / numbers.length;
+// }
+
+// let exampleGrades = [15, 18, 12, 20]; 
+// let average = calculateAverage(exampleGrades); 
+// console.log("The average of the grades is: " + average); 
+
 function calculateAverage(numbers) {
     let total = 0; 
     for (let i = 0; i < numbers.length; i++) {
         total += numbers[i]; 
     }
-    return total / numbers.length;
+    return total / numbers.length; 
 }
 
 let exampleGrades = [15, 18, 12, 20]; 
 let average = calculateAverage(exampleGrades); 
-console.log("The average of the grades is: " + average); 
+console.log("The average of the grades is: " + average);
 
